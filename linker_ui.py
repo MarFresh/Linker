f15d39be8a5bce9122a63796534004f9d5493ad8
MarFresh <marreymbi@gmail.com>
2023-06-16 07:37:07 -0400

    Update linker_ui.py

--- linker_ui.py
+++ linker_ui.py
@@ -1,6 +1,15 @@
 from customtkinter import *
+from tkinter import *
 
 class LinkerUi:
   def __init__(self):
     pass
-    
+  def login(self):
+    login=CTk()
+    title=Label(login, text="LinkedIn Credentials").pack()
+    e=CTkEntry(login, placeholder="Linked Email").pack()
+    p=CTkEntry(login, placeholder="Linked Password").pack()
+    submit=CTkButton(login, text="Login", command=send).pack()
+    login.mainloop()
+  def send(self):
+    pass
